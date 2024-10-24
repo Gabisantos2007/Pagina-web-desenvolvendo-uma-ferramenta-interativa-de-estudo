575473824d7a05b62964ce17adba6c293980b29f
Gabisantos2007 <141463743+Gabisantos2007@users.noreply.github.com>
2024-10-23 22:06:16 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Ama fazer',
-    'Que vocẽ gosta de comer?',
+    'Comida',
+    'O que vocẽ gosta de comer?',
     'lazanha'
 )
 
 criaCartao(
-    'Te deixa triste',
-    'Fica longe da kamila',
-    '@Gabi santos'
+    'Sentimentos'
+    'Você sabe que me deixa triste?',
+    'Ficar longe da kamila'
 )
 
 criaCartao(
-    'Professor de informatica e legal',
-    ' Sim o rafael é muito prestativo, inteligente muito dedicado nos alunos',
-    'yes'
+    'Melhor professor de Informática',
+    'Quem é o melhor professor de informática?',
+    'Professor Rafael Assis'
 )
 
 criaCartao(
-    'Trabalha em que',
-    'Quem vende produtos oboticario ',
-    ' Gabizinha'
+    'Carro',
+    'Qual é o carro mais lindo do mundo',
+    'Jetta'
 )
